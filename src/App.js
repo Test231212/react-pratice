d6fbe3c20b7a36997d9a3fe2a393863de222fcff
test231212 <j910219@gmail.com>
2024-07-01 11:23:50 +0900

    상태관리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Header from './components/Header';
+import Body from './components/Body';
+import Body2 from './components/Body2';
+import Post from './components/Post';
+
 
 function App() {
   return (
     <div>
       <Header/>
-      <h1>안녕</h1>
-      <div className="box">박스</div>
+      <Body/>
+      <Body2 username={"ssar"} password={"1234"} />
+      <Post/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,24 @@ import styled from 'styled-components';
 const MyLi = styled.li`
 color: green;
 font-size: 30px;
+list-style-type: none; // 점3개 사라짐
+`;
+
+const MyUl = styled.ul`
+    display: flex;
+    color: green;
+font-size: 30px;
+list-style-type: none; // 점3개 사라짐
 `;
 
 function Header() {
     return (
         <div>
-            <ul>
+            <MyUl>
                 <MyLi>홈</MyLi>
                 <MyLi>글쓰기</MyLi>
                 <MyLi>로그아웃</MyLi>
-            </ul>
+            </MyUl>
         </div>
     );
 }
